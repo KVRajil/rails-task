84f21c631a88624cbdd6efc24c19492073ded565
RajilKV <rajil.kv@constantcontact.com>
2023-08-25 13:23:28 +0530

    add model validtions and scope

--- app/models/fibonacci.rb
+++ app/models/fibonacci.rb
@@ -1,2 +1,8 @@
 class Fibonacci < ApplicationRecord
+
+  validates :value, :result, :runtime, presence: true
+  validates :value, :result, numericality: { only_integer: true }
+  validates :runtime, numericality: true
+
+  scope :recent, -> { order(created_at: :desc) }
 end
